31b1e686cf2c322f1e6a068aaf78a987e3235c2b
Alyssa Dai <alyssa.ydai@gmail.com>
2026-02-19 14:20:33 -0500

    validate that NAME is not whitespace-only

--- configure_nb/federation_nodes_model.py
+++ configure_nb/federation_nodes_model.py
@@ -1,6 +1,13 @@
 from typing import Annotated
 
-from pydantic import BaseModel, Field, HttpUrl, RootModel
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field,
+    HttpUrl,
+    RootModel,
+    field_validator,
+)
 
 
 class InternalFederationNode(BaseModel):
@@ -13,6 +20,19 @@ class InternalFederationNode(BaseModel):
         HttpUrl,
         Field(validation_alias="API_URL", serialization_alias="ApiURL"),
     ]
+
+    model_config = ConfigDict(extra="forbid")
+
+    @field_validator("name")
+    @classmethod
+    def check_name_not_whitespace(cls, value: str) -> str:
+        """
+        Raise an error (will be caught as a ValidationError by Pydantic) if
+        the required field is an empty string or all whitespace.
+        """
+        if value.strip() == "":
+            raise ValueError("Field cannot be an empty string.")
+        return value
 
 
 class InternalFederationNodes(RootModel[list[InternalFederationNode]]):
